9fab6edfd265d8da004d2bbee58cd621a7b66602
DortCeL <exh.alif.47@gmail.com>
2024-08-22 11:54:41 +0600

    localhost ar vercel er jonno alada URL dewar kotha

--- client/config.js
+++ client/config.js
@@ -1,2 +1,12 @@
 // export const backendUrl = `http://localhost:3000`;
-export const backendUrl = `https://workout-buddy-backend-one.vercel.app`;
+// export const backendUrl = `https://workout-buddy-backend-one.vercel.app`;
+
+let backendUrl;
+
+if (window.location.hostname === "localhost") {
+	backendUrl = "http://localhost:3000";
+} else {
+	backendUrl = "https://workout-buddy-backend-one.vercel.app";
+}
+
+export { backendUrl };
